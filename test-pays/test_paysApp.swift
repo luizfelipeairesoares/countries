377d7b3814e135407149cce31a707ada48dce72f
Luiz Aires Soares <lfelipeas@gmail.com>
2025-03-03 15:52:22 -0500

    Initial Commit

---
+++ test-pays/test_paysApp.swift
@@ -0,0 +1,17 @@
+//
+//  test_paysApp.swift
+//  test-pays
+//
+//  Created by Luiz Aires Soares on 2025-03-03.
+//
+
+import SwiftUI
+
+@main
+struct test_paysApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ test-paysTests/test_paysTests.swift
@@ -0,0 +1,16 @@
+//
+//  test_paysTests.swift
+//  test-paysTests
+//
+//  Created by Luiz Aires Soares on 2025-03-03.
+//
+
+import Testing
+
+struct test_paysTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
