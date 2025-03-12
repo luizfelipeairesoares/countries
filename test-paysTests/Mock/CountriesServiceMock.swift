06a5dff4d15cdf249ec7f0bfa7bda64859f737b5
Luiz Aires Soares <lfelipeas@gmail.com>
2025-03-12 12:33:41 -0400

    Updates mock class due to protocol changes.

--- test-paysTests/Mock/CountriesServiceMock.swift
+++ test-paysTests/Mock/CountriesServiceMock.swift
@@ -18,20 +18,41 @@ class CountriesServiceMock: CountriesServiceProtocol {
         if !mockFailRequest {
             completion(.success(countries))
         } else {
-            let object = ErrorResponseObject(status: 503, message: errorMessage)
-            let error = NetworkError.error(object)
-            completion(.failure(error))
+
+            completion(.failure(createNetworkError(code: 503)))
         }
     }
-    
+
+    func listAll() async throws -> [Country] {
+        if !mockFailRequest {
+            return countries
+        } else {
+            throw createNetworkError(code: 503)
+        }
+    }
+
+    func listAllConcurrently() async throws -> [Country] {
+        if !mockFailRequest {
+            return countries
+        } else {
+            throw createNetworkError(code: 503)
+        }
+    }
+
     func searchCountry(name: String, completion: @escaping (Result<[Country], NetworkError>) -> Void) {
         if !mockFailRequest {
             completion(.success(countries))
         } else {
-            let object = ErrorResponseObject(status: 503, message: errorMessage)
-            let error = NetworkError.error(object)
-            completion(.failure(error))
+            completion(.failure(createNetworkError(code: 503)))
         }
+    }
+
+    // MARK: - Private
+
+    private func createNetworkError(code: Int) -> NetworkError {
+        let object = ErrorResponseObject(status: code, message: errorMessage)
+        let error = NetworkError.error(object)
+        return error
     }
 
 }
