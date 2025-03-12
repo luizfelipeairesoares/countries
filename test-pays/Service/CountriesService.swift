c75c3e571e4c4147cdfd24afb405f4de98f6c563
Luiz Aires Soares <lfelipeas@gmail.com>
2025-03-12 12:34:35 -0400

    Updates protocol adding functions for SwiftConcurrency

--- test-pays/Service/CountriesService.swift
+++ test-pays/Service/CountriesService.swift
@@ -10,7 +10,8 @@ import Combine
 protocol CountriesServiceProtocol: NetworkServiceProtocol {
 
     func listAll(completion: @escaping (Result<[Country], NetworkError>) -> Void)
-    func listAllConcurrently(completion: @escaping (Result<[Country], NetworkError>) -> Void) async throws
+    func listAll() async throws -> [Country]
+    func listAllConcurrently() async throws -> [Country]
     func searchCountry(name: String, completion: @escaping (Result<[Country], NetworkError>) -> Void)
 
 }
@@ -24,6 +25,7 @@ class CountriesService: CountriesServiceProtocol {
         self.provider = provider
     }
 
+    @available(*, renamed: "listAll()")
     func listAll(completion: @escaping (Result<[Country], NetworkError>) -> Void) {
         provider.request(endpoint: RestCountriesAPI.all)
             .map { $0 }
@@ -33,30 +35,53 @@ class CountriesService: CountriesServiceProtocol {
                 case .failure(let error):
                     completion(.failure(error))
                 case .finished:
-                     break
+                    break
                 }
             } receiveValue: { response in
                 completion(.success(response))
             }
             .store(in: &cancellables)
+//        Task {
+//            do {
+//                let result = try await listAll()
+//                completion(.success(result))
+//            } catch {
+//                completion(.failure(error as! NetworkError))
+//            }
+//        }
+    }
+    
+    
+    func listAll() async throws -> [Country] {
+        return try await withCheckedThrowingContinuation { continuation in
+            provider.request(endpoint: RestCountriesAPI.all)
+                .map { $0 }
+                .receive(on: DispatchQueue.main)
+                .sink { receiveCompletion in
+                    switch receiveCompletion {
+                    case .failure(let error):
+                        continuation.resume(with: .failure(error))
+                    case .finished:
+                        break
+                    }
+                } receiveValue: { response in
+                    continuation.resume(with: .success(response))
+                }
+                .store(in: &cancellables)
+        }
     }
 
-    func listAllConcurrently(completion: @escaping (Result<[Country], NetworkError>) -> Void) async throws {
+    func listAllConcurrently() async throws -> [Country] {
         do {
             let response: [Country] = try await provider.request(endpoint: RestCountriesAPI.all)
-            completion(.success(response))
+            return response
         } catch {
-            if let networkError = error as? NetworkError {
-                completion(.failure(networkError))
-            } else {
-                let networkError = NetworkError.encodingError(error)
-                completion(.failure(networkError))
-            }
+            throw error
         }
     }
 
     func searchCountry(name: String, completion: @escaping (Result<[Country], NetworkError>) -> Void) {
-        return provider.request(endpoint: RestCountriesAPI.search(name: name))
+        provider.request(endpoint: RestCountriesAPI.search(name: name))
             .map { $0 }
             .receive(on: DispatchQueue.main)
             .sink(receiveCompletion: { receiveCompletion in
